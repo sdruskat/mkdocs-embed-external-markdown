d4aa286b8b273e19ba2914e666fcd7b3f4794758
Druskat, Stephan <stephan.druskat@dlr.de>
2023-11-27 10:08:43 +0100

    Add regression tests for different types of links

--- tests/external-markdown/test_plugin.py
+++ tests/external-markdown/test_plugin.py
@@ -1,42 +1,39 @@
-from pytest import fixture
-from requests import Response
+import pytest
 
 from external_markdown.plugin import EmbedExternalMarkdown
 
-
-@fixture
-def markdown():
-    return """# Test links
-## Links that should remain intact
-- [external link](https://github.com)
-- [external link with anchor](https://ansible-docs.readthedocs.io/zh/stable-2.0/rst/playbooks_variables.html#using-variables-about-jinja2)
-- [internal link](#links)
-- [site-internal anchored link](testpage.md#test-subsection)
-- [site-internal link](testpage.md)"""
+BASE_URL = "https://BASEURL/"
+BASE_FILE = "https://BASEURL/FILE.md"
+TEST_DATA = [
+    (
+        "external link",
+        "https://github.com",
+        "https://github.com"),
+    (
+        "external link with anchor",
+        "https://ansible-docs.readthedocs.io/zh/stable-2.0/rst/playbooks_variables.html#using-variables-about-jinja2",
+        "https://ansible-docs.readthedocs.io/zh/stable-2.0/rst/playbooks_variables.html#using-variables-about-jinja2",
+    ),
+    (
+        "anchor",
+        "#links",
+        "#links"),
+    (
+        "local link",
+        "page.md",
+        f"{BASE_URL}page.md"),
+    (
+        "local link with anchor",
+        "page.md#test-subsection",
+        f"{BASE_URL}page.md#test-subsection",
+    ),
+]
 
 
 class TestEmbedExternalMarkdown:
-    def test_keep_anchors(self, mocker, markdown):
-        plugin = "external_markdown.plugin.EmbedExternalMarkdown"
-        mocker.patch(f"{plugin}.is_valid_url", return_value=True)
-        mocker.patch(f"{plugin}.make_request", return_value=Response())
-        mocker.patch(f"{plugin}.get_markdown_from_response", return_value=markdown)
-        # mocker.patch(
-        #     f"{plugin}.update_relative_links",
-        # )
-        converted_md = EmbedExternalMarkdown().external_markdown(
-            "https://BASEURL/FILE.md"
-        )
-
-        print(f"\n\n\n{converted_md}\n\n\n")
-
-        assert "[external link](https://github.com)" in converted_md
-        assert (
-            "[external link with anchor](https://ansible-docs.readthedocs.io/zh/stable-2.0/rst/playbooks_variables"
-            ".html#using-variables-about-jinja2)" in converted_md
-        )
-        assert "[internal link](#links)" in converted_md
-        assert (
-            "[semi-internal anchored link](testpage.md#test-subsection)" in converted_md
-        )
-        assert "[semi-internal link](testpage.md)" in converted_md
+    @pytest.mark.parametrize("label,link_url,expected_url", TEST_DATA)
+    def test_update_relative_links_external(self, label, link_url, expected_url):
+        # Regression tests for #11
+        link = f"[{label}]({link_url})"
+        expected = f"[{label}]({expected_url})"
+        assert EmbedExternalMarkdown().update_relative_links(link, BASE_FILE) == expected
